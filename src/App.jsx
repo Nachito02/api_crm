bc0ddfeadc8983a8baf41285c753a0e015dfc659
Juan Arguello <arguellojuan08@gmail.com>
2022-01-27 06:04:43 -0300

    Tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,17 +15,21 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-       
 
-        <Route path="/clientes" element={<Layout />}>
+  
+          <Route path='/' element={<Layout />} > </Route>
+
+         
+
+          <Route path="/clientes" element={<Layout />}>
+
           <Route index element={<Inicio />} />
           <Route path="nuevo" element={<NuevoCliente />} />
           <Route path="editar/:id" element={<EditarCliente />} />
           <Route path=":id" element={<VerCliente />} />
 
-
-
         </Route>
+       
       </Routes>
     </BrowserRouter>
   );
